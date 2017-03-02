1961e259bf4e9a476fe04447724476f0fc0b46ca
Mees van Dijk <mees@itisme.es>
2017-03-02 02:42:49 +0100

    changed naming of tests

--- src/structures/ArrayTemplate.spec.js
+++ src/structures/ArrayTemplate.spec.js
@@ -8,21 +8,35 @@ const simplePlain = [{
   type: 'string'
 }]
 
-test('doesn\'t fail on creation', () => {
+test('creates single-element template', () => {
   // it shouldn't fail on creation
   // eslint-disable-next-line no-new
   new ArrayTemplate(simplePlain)
 })
 
-test('should create field', t => {
+test('creates field', t => {
   const template = new ArrayTemplate(simplePlain)
   t.true(template.field instanceof Field)
 })
 
-test('should throw on multiple-element array', t => {
+test('throws on multiple-element array', t => {
   t.throws(() => {
     // eslint-disable-next-line no-new
     new ArrayTemplate([{}, {}, {}])
   })
 })
 
+test('passes single-element, correct data', t => {
+  const template = new ArrayTemplate(simplePlain)
+  t.true(template.test(['str']))
+})
+
+test('does not pass single-element incorrect data', t => {
+  const template = new ArrayTemplate(simplePlain)
+  t.false(template.test([1]))
+})
+
+test('passes multiple-element correct data', t => {
+  const template = new ArrayTemplate(simplePlain)
+  t.true(template.test(['str1', 'str2', 'str3']))
+})
